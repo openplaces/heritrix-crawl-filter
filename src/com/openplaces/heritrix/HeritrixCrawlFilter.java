8f11df5bfe307403c79f74019358a2200cc32579
Greg Lu <greg.lu@gmail.com>
2010-05-12 18:47:48 -0400

    Added dependencies

---
+++ src/com/openplaces/heritrix/HeritrixCrawlFilter.java
@@ -0,0 +1,21 @@
+package com.openplaces.heritrix;
+import org.archive.crawler.framework.Scoper;
+import org.archive.modules.CrawlURI;
+
+public class HeritrixCrawlFilter extends Scoper {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void innerProcess(CrawlURI arg0) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean shouldProcess(CrawlURI arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}

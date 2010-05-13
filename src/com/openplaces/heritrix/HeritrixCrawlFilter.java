8854a27e6f742498d62d55182d56ddb143a92159
Greg Lu <greg.lu@gmail.com>
2010-05-12 20:28:22 -0400

    Almost there...

--- src/com/openplaces/heritrix/HeritrixCrawlFilter.java
+++ src/com/openplaces/heritrix/HeritrixCrawlFilter.java
@@ -1,21 +1,62 @@
 package com.openplaces.heritrix;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import static org.archive.modules.fetcher.FetchStatusCodes.S_OUT_OF_SCOPE;
 import org.archive.crawler.framework.Scoper;
 import org.archive.modules.CrawlURI;
+import org.archive.modules.ProcessResult;
 
 public class HeritrixCrawlFilter extends Scoper {
 
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void innerProcess(CrawlURI arg0) throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+	private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+
+	private Map<String, String> seedFiltersMap;
+	private Set<String> seedSet;
+
+	public HeritrixCrawlFilter() {
+	}
+
+	public Map<String, String> getSeedFilters() {
+		return seedFiltersMap;
+	}
+
+	public void setSeedFiltersMap(Map<String, String> seedFiltersMap) {
+		LOGGER.info(seedFiltersMap.toString());
+
+		this.seedFiltersMap = seedFiltersMap;
+		this.seedSet = seedFiltersMap.keySet();
+	}
+
+	public Set<String> getSeedSet() {
+		return seedSet;
 	}
 
 	@Override
-	protected boolean shouldProcess(CrawlURI arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	protected void innerProcess(CrawlURI curi) throws InterruptedException {
 	}
+
+	@Override
+	protected boolean shouldProcess(CrawlURI curi) {
+		return getSeedSet().contains(curi.getPathFromSeed());
+	}
+
+	@Override
+    protected ProcessResult innerProcessResult(CrawlURI curi) throws InterruptedException {
+		String seedPath = curi.getPathFromSeed();
+		String filterString = getSeedFilters().get(seedPath);
+
+		if (!Pattern.matches(filterString, curi.toString())) {
+			curi.setFetchStatus(S_OUT_OF_SCOPE);
+			return ProcessResult.FINISH;
+		}
+
+		return ProcessResult.PROCEED;
+    }
 	
 }
